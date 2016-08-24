90cd64de70185d10afd62442f636eff3eb98955a
Camron Khan <camron.khan@gmail.com>
2016-08-24 09:45:32 -0500

    changed schema type to sql

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Supportview
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Postgres-specific schema dump
+    config.active_record.schema_format = :sql
   end
 end
