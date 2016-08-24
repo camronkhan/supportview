dc5feee6eed0157496ee75c47720094c83123853
Camron Khan <camron.khan@gmail.com>
2016-08-24 11:18:33 -0500

    added company, source, and product seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+@num_companies = 200
+@num_sources = 10
+@num_products = 2000
+
+@num_companies.times do |i|
+	Company.create!(
+		name: "#{Faker::Company.name}#{i}",
+		website_url: Faker::Internet.url
+	)
+end
+
+@num_sources.times do |i|
+	Source.create!(
+		location: "#{Faker::Address.city}#{i}"
+	)
+end
+
+@num_products.times do |i|
+	Product.create!(
+		name: "#{Faker::Commerce.product_name}#{i}",
+		description: Faker::Lorem.sentence,
+		image_url: Faker::Avatar.image,
+		company_id: rand(1..@num_companies),
+		source_id: rand(1..@num_sources)
+	)
+end
