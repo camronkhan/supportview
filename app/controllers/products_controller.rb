b7555c3eecf50f013fce6be65691d6758cbbaa27
Camron Khan <camron.khan@gmail.com>
2016-08-24 12:25:21 -0500

    added nav, footer, and search functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,12 @@
 class ProductsController < ApplicationController
+
 	def index
-		@products = Product.all.limit(10)
+		if params[:keywords].present?
+			@keywords = params[:keywords]
+			product_search_term = ProductSearchTerm.new(@keywords)
+			@products = Product.where(product_search_term.where_clause, product_search_term.where_args).order(product_search_term.order).limit(10)
+		else
+			@products = Product.all.limit(10)
+		end
 	end
 end
